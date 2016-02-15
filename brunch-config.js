ac675117f1bb98388f2ba12e4164d367c3727f65
ChrisBarthol <Chris.Barthol@gmail.com>
2016-02-15 16:25:18 -0500

    added bower to get jquery into project for bootstrpa downdown menu

--- brunch-config.js
+++ brunch-config.js
@@ -52,6 +52,11 @@ exports.config = {
     babel: {
       // Do not use ES6 compiler in vendor code
       ignore: [/web\/static\/vendor/]
+    },
+    sass: {
+      options: {
+        includePaths: ['bower_components/bootstrap-sass/assets/stylesheets']
+      }
     }
   },
 
