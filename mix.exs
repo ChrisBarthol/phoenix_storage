8891bf2906993a7a434d35979d202a3fba4ccd8b
ChrisBarthol <Chris.Barthol@gmail.com>
2016-02-29 20:35:21 -0500

    mix hex.outdated is awesome

--- mix.exs
+++ mix.exs
@@ -29,15 +29,17 @@ defmodule Storage.Mixfile do
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
+
+  #mix hex.outdated to see latest version
   defp deps do
     [{:phoenix, "~> 1.1.4"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_ecto, "~> 2.0"},
-     {:phoenix_html, "~> 2.4"},
+     {:postgrex, ">= 0.11.1"},
+     {:phoenix_ecto, "3.0.0-beta.2"},
+     {:phoenix_html, "~> 2.5"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.9"},
-     {:cowboy, "~> 1.0"},
-     {:comeonin, "~> 2.1"}]
+     {:gettext, "~> 0.10"},
+     {:cowboy, "~> 1.0.4"},
+     {:comeonin, "~> 2.1.1"}]
   end
 
   # Aliases are shortcut or tasks specific to the current project.
