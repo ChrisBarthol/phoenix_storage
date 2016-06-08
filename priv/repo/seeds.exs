1a83ccc8134f2097163d65b7dbf88b937a1c90a9
ChrisBarthol <Chris.Barthol@gmail.com>
2016-06-08 12:53:39 -0400

    lunchtime work on seeds file

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,87 +10,52 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-users = [
-    {
-        "username":     "user01@test.com",
-        "first_name":   "Robot",
-        "last_name":    "1",
-        "password":     "1234",
-        "phone_number": "",
-        "role":         "admin"
-    },
-    {
-        "username":     "user02@test.com",
-        "first_name":   "Robot",
-        "last_name":    "2",
-        "password":     "5678",
-        "phone_number": "",
-        "role":         "batman"
-    },
-    {
-        "username":     "user03@test.com",
-        "first_name":   "Robot",
-        "last_name":    "3",
-        "phone_number": "",
-        "role":         "user"
-    }
-]
+alias Storage.Repo
+alias Storage.Container
+alias Storage.User
 
-add_objects(users, User)
 
-locations = [
-    {
-        "name": "He-Man's Shack",
-        "address": "2138 Triad Court",
-        "city": "Columbus",
-        "zip_code": 43235,
-        "state": "OH"
-    }
-]
-
-add_objects(locations, Location)
-
-u = db.session.query(User).first()
-u.locations.append(db.session.query(Location).first())
-db.session.add(u)
-db.session.commit()
-
-substances = [
-    {
-        "name": "Milk",
-        "density": 1.035,
-        "caloric_censity": 6.2
-    }
-]
-
-add_objects(substances, Substance)
-
-containers = [
-    {
-        "name": "Milk",
-        "place": "kitchen",
-        "full_amount": 100,
-        "warning_amount": 30,
-        "location_id": db.session.query(Location).first().id,
-        "substance_id": db.session.query(Substance).first().id
-    }
-
-]
-
-add_objects(containers, Container)
-
-u = db.session.query(User).first()
-u.containers.append(db.session.query(Container).first())
-db.session.add(u)
-db.session.commit()
-
-defmodule Seeds do
-  #start import
-  def import_data(users,locations, substances, containers) do
-    import_users users
-  end
-
-  defp import_users([{user}|t]) do
-    Storage.Repo.insert!(User{user}, log: false)
-  end
+def populate_users(n) do
+  Repo.insert!(%User{
+      username: "user#{n}test.com",
+      first_name: "Robot",
+      last_name: "#{n}",
+      password: "testtesttest#{n}",
+      phone_number: "",
+      role: "user"
+  })
+  populate_users(n-1)
 end
+
+def populate_locations(n) do
+  Repo.insert!(%Location{
+      name: "Shack #{n}",
+      address: "123 #{n}",
+      zip_code: 43035,
+      state: "OH"
+  })
+  populate_locations(n-1)
+end
+
+def populate_substances(n) do
+  Repo.insert!(%Substance{
+      name: "Super Ingredient #{n}",
+      density: 1,
+      caloric_censity: 3
+  })
+end
+
+def populate_containers(n) do
+  Repo.insert!(%Container{
+      name: "Container #{n}",
+      place: "kitchen",
+      full_amount: 100,
+      warning_amount: 30
+  })
+end
+populate_users(10)
+populate_locations(4)
+populate_substances(10)
+popuate_containers(5)
+
+#Need to add relationships
